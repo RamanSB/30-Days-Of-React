9b6a84af78de5af009a4d61b5fb210efc2495aff
RamanSB <ramansb100@gmail.com>
2021-11-12 18:33:21 +0000

    exercise 3a before functions

--- solutions/day-01/level4.js
+++ solutions/day-01/level4.js
@@ -25,7 +25,7 @@ dog.getDogInfo = function() {
 
 // level 2
 // #1
-const users = {
+const users1 = {
     Alex: {
       email: 'alex@alex.com',
       skills: ['HTML', 'CSS', 'JavaScript'],
@@ -77,11 +77,11 @@ const users = {
     }
   };
   
-  console.log(`Object entries: ${Object.entries(users)}`);
+  console.log(`Object entries: ${Object.entries(users1)}`);
   let highNoOfSkills = 0;
-  for (let key in users) {
-    if (users[key].skills.length > highNoOfSkills) {
-        highNoOfSkills = users[key].skills.length;
+  for (let key in users1) {
+    if (users1[key].skills.length > highNoOfSkills) {
+        highNoOfSkills = users1[key].skills.length;
         console.log(`Highest skillset is currently: ${key} with ${highNoOfSkills} skills`);
     }
   }
@@ -89,11 +89,11 @@ const users = {
   // #2
   let count = 0;
 let gte50Pts = 0;
-  for (let key in users) {
-      if(users[key].isLoggedIn) {
+  for (let key in users1) {
+      if(users1[key].isLoggedIn) {
         count++;
       }
-      if(users[key].points >= 50) {
+      if(users1[key].points >= 50) {
         gte50Pts++;
       }
   }
@@ -103,8 +103,8 @@ let gte50Pts = 0;
 
   // #3
   const mernStackers = [];
-  for(let key in users) {
-      let skillsArray = users[key].skills;
+  for(let key in users1) {
+      let skillsArray = users1[key].skills;
       if(skillsArray.includes('MongoDB') && skillsArray.includes('Express') && skillsArray.includes('Node') && skillsArray.includes("React")) {
           mernStackers.push(key);
       }
@@ -116,9 +116,176 @@ let gte50Pts = 0;
    let updatedUsers = Object.assign({"Ramandeep": {
        "email": "ramansb100@gmail.com",
        "skills": ["Java", "Javascript", "React", "HTML", "CSS", "MongoDB", "Node", "Python", "SQL"]
-   }}, users);
+   }}, users1);
 
-   console.log(JSON.stringify(updatedUsers));
+console.log(JSON.stringify(updatedUsers));
+
 // #5 & 6
-   console.log(Object.keys(updatedUsers));
-   console.log(Object.values(updatedUsers));
+console.log(Object.keys(updatedUsers));
+console.log(Object.values(updatedUsers)); 
+
+// level 3
+// #1
+const personAccount = {
+    firstName: "Ramandeep",
+    lastName: "Toasti",
+    incomes: [
+        {
+            income: 40000,
+            description: "Software Dev"
+        },
+        {
+            income: 10000,
+            description: "Piano Gigs"
+        }
+    ],
+    expenses: [
+        {
+            exepense: 15000,
+            description: "Tax"
+        },
+        {
+            expense: 10000,
+            description: "Helping out at home"
+        }
+    ],
+    totalIncome: 50000,
+    totalExpenses: 25000,
+    accountInfo: {
+        sortCode: "20-67-90",
+        accountNo: "12510472"
+    },
+    addIncome: function(amountToAdd) {
+        this.totalIncome = amountToAdd + this.totalIncome;
+    },
+    addExpense: function(amountToAdd) {
+        this.totalExpenses = amountToAdd + this.totalExpenses;
+    }
+};
+
+personAccount.addIncome(500);
+console.log(personAccount);
+
+const users = [
+    {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt: '08/01/2020 9:00 AM',
+      isLoggedIn: false,
+    },
+    {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt: '08/01/2020 9:30 AM',
+      isLoggedIn: true,
+    },
+    {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt: '08/01/2020 9:45 AM',
+      isLoggedIn: true,
+    },
+    {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt: '08/01/2020 9:50 AM',
+      isLoggedIn: false,
+    },
+    {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt: '08/01/2020 10:00 AM',
+      isLoggedIn: false,
+    },
+  ]
+  
+  const products = [
+    {
+      _id: 'eedfcf',
+      name: 'mobile phone',
+      description: 'Huawei Honor',
+      price: 200,
+      ratings: [
+        { userId: 'fg12cy', rate: 5 },
+        { userId: 'zwf8md', rate: 4.5 },
+      ],
+      likes: [],
+    },
+    {
+      _id: 'aegfal',
+      name: 'Laptop',
+      description: 'MacPro: System Darwin',
+      price: 2500,
+      ratings: [],
+      likes: ['fg12cy'],
+    },
+    {
+      _id: 'hedfcg',
+      name: 'TV',
+      description: 'Smart TV:Procaster',
+      price: 400,
+      ratings: [{ userId: 'fg12cy', rate: 5 }],
+      likes: ['fg12cy'],
+    },
+  ]
+
+  // #2a
+  function signUp(user) {
+      let totalNoOfUsers = users.length;
+      for(let existingUser of users) {
+        if (existingUser.username == user.username) {
+            console.log(`User: ${user.username} already exists.`);
+        }
+      };
+      const requiredProperties = Object.keys(users[0]).unshift(); //Removing _id as mongo db will add this upon entry/insertion
+      for (let prop in requiredProperties) {
+            let result = Object.keys(user).includes(prop);
+            if(!result) {
+                console.log(`Unknown property detected: ${prop}`);
+            }
+      }
+      users.push(user);
+  }
+console.log(users);
+signUp({});
+signUp({username: 'Thomas'});
+signUp({      
+    username: 'Jacob',
+    email: 'jacob@lemarc.com',
+    password: '123333',
+    createdAt: '08/01/2020 10:00 AM',
+    isLoggedIn: false
+});
+console.log(users);
+
+// #2b
+function signIn(user) {
+    for (let existingUser of users) { 
+        console.log(existingUser);
+        if (existingUser.username == user.username && existingUser.password == user.password){  // In an actual signin method hashing (bcryptjs) would be required to decrypt the password read in from db (as it woudl already be hashed)
+                console.log(`User: ${user.username} has successfully signed in.`);
+            }  
+        }
+}
+
+signIn(
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+    }
+);
+
+
+// #3
